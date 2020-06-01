c5cb75c776ee173736362ee84932c1a785352f3c
PAHPDESKTOP\Pablo <pablo@otakupahp.com>
2020-06-01 15:21:17 -0500

    Add Singleton instance

--- License.php
+++ License.php
@@ -4,314 +4,358 @@ namespace LMFW\SDK;
 
 use ErrorException;
 
-/**
- * License Manager for WooCommerce SDK to let communication with the API
- *
- * Defines basic functionality to connect with the API
- *
- * @since      1.0.0
- * @package    Pahp/SDK
- * @subpackage License
- * @author     Pablo Hernández (OtakuPahp) <pablo@otakupahp.com>
- */
-class License {
+if( !class_exists('LMFW\SDK\License') ) {
 
-	/**
-	 * @since 1.0.0
-	 * @access private
-	 * @var string
-	 */
-	private $plugin_name;
+    /**
+     * License Manager for WooCommerce SDK to let communication with the API
+     *
+     * Defines basic functionality to connect with the API
+     *
+     * @since      1.0.0
+     * @package    Pahp/SDK
+     * @subpackage License
+     * @author     Pablo Hernández (OtakuPahp) <pablo@otakupahp.com>
+     */
+    class License
+    {
 
-	/**
-	 * @since 1.0.0
-	 * @access private
-	 * @var string $api_url
-	 */
-	private $api_url;
+        /**
+         * @since 2.0.0
+         * @access private
+         * @var $instance
+         */
+        private static $instance = null;
 
-	/**
-	 * @since 1.0.0
-	 * @access private
-	 * @var string $customer_key
-	 */
-	private $customer_key;
+        /**
+         * @since 1.0.0
+         * @access private
+         * @var string
+         */
+        private $plugin_name;
 
-	/**
-	 * @since 1.0.0
-	 * @access private
-	 * @var string $customer_secret
-	 */
-	private $customer_secret;
+        /**
+         * @since 1.0.0
+         * @access private
+         * @var string $api_url
+         */
+        private $api_url;
 
-	/**
-	 * @since 1.0.0
-	 * @access private
-	 * @var array $valid_status
-	 */
-	private $valid_status;
+        /**
+         * @since 1.0.0
+         * @access private
+         * @var string $customer_key
+         */
+        private $customer_key;
 
-	/**
-	 * @since 1.0.0
-	 * @access private
-	 * @var array
-	 */
-	private $product_ids;
+        /**
+         * @since 1.0.0
+         * @access private
+         * @var string $customer_secret
+         */
+        private $customer_secret;
 
-	/**
-	 * @since 1.0.0
-	 * @access private
-	 * @var string
-	 */
-	private $stored_license;
-	
-	/**
-	 * @since 1.0.0
-	 * @access private
-	 * @var string
-	 */
-	private $valid_object;
-	
-	/**
-	 * @since 1.0.0
-	 * @access private
-	 * @var int
-	 */
-	private $ttl;
+        /**
+         * @since 1.0.0
+         * @access private
+         * @var array $valid_status
+         */
+        private $valid_status;
 
-	/**
-	 * License constructor
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param string $plugin_name
-	 * @param string $server_url
-	 * @param string $customer_key
-	 * @param string $customer_secret
-	 * @param mixed $product_ids
-	 * @param array $license_options
-	 * @param string $valid_object
-	 * @param int $ttl
-	 */
-	public function __construct(
-		$plugin_name, 
-		$server_url, 
-		$customer_key, 
-		$customer_secret, 
-		$product_ids,
-		$license_options,
-		$valid_object,
-		$ttl
-	) {
+        /**
+         * @since 1.0.0
+         * @access private
+         * @var array
+         */
+        private $product_ids;
 
-		# Set plugin name for internationalization
-		$this->plugin_name = $plugin_name;
+        /**
+         * @since 1.0.0
+         * @access private
+         * @var string
+         */
+        private $stored_license;
 
-		# Connection variables
-		$this->api_url         = "{$server_url}/wp-json/lmfwc/v2/";
-		$this->customer_key    = $customer_key;
-		$this->customer_secret = $customer_secret;
-		
-    		# Product IDs
-		$this->product_ids =  is_array( $product_ids ) ? $product_ids : [$product_ids];
-		
-    		# Get license key stored in the database
-		$this->stored_license = null;
-		if( isset($license_options['settings_key']) ) {  // Check if WP Settings are used to store the license key
-			$license = get_option($license_options['settings_key']);
-			if($license !== false) {
-				$this->stored_license = $license[$license_options['option_key']];
-			}
-		}
-		elseif( isset($license_options['option_key']) ) {  // If no WP Settings are used to store the license key
-			$this->stored_license = get_option($license_options['option_key']);
-		}
-		
-		# License variables
-		$this->valid_object = $valid_object;
-		$this->ttl = $ttl;
-		$this->valid_status = get_option($valid_object, []);
+        /**
+         * @since 1.0.0
+         * @access private
+         * @var string
+         */
+        private $valid_object;
 
-	}
+        /**
+         * @since 1.0.0
+         * @access private
+         * @var int
+         */
+        private $ttl;
 
-	/**
-	 * HTTP Request call
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param string $endpoint
-	 * @param string $method
-	 * @param string $args
-	 *
-	 * @return array
-	 * @throws ErrorException
-	 */
-	private function call($endpoint, $method = 'GET', $args = '' ) {
+        /**
+         * The singleton instance
+         *
+         * @param string $plugin_name
+         * @param string $server_url
+         * @param string $customer_key
+         * @param string $customer_secret
+         * @param mixed $product_ids
+         * @param array $license_options
+         * @param string $valid_object
+         * @param int $ttl
+         * @return License
+         * @since 2.0.0
+         *
+         */
+        public static function get_instance(
+            $plugin_name,
+            $server_url,
+            $customer_key,
+            $customer_secret,
+            $product_ids,
+            $license_options,
+            $valid_object,
+            $ttl
+        )
+        {
+            if (self::$instance == null) {
+                self::$instance = new License($plugin_name, $server_url, $customer_key, $customer_secret, $product_ids, $license_options, $valid_object, $ttl);
+            }
 
-		# Populate the correct endpoint for the API request
-		$url = "{$this->api_url}{$endpoint}?consumer_key={$this->customer_key}&consumer_secret={$this->customer_secret}";
+            return self::$instance;
+        }
 
-		# Create header
-		$headers = [
-			'Accept'        => 'application/json',
-			'Content-Type'  => 'application/json; charset=UTF-8',
-		];
+        /**
+         * License constructor
+         *
+         * @param string $plugin_name
+         * @param string $server_url
+         * @param string $customer_key
+         * @param string $customer_secret
+         * @param mixed $product_ids
+         * @param array $license_options
+         * @param string $valid_object
+         * @param int $ttl
+         * @since 1.0.0
+         *
+         */
+        public function __construct(
+            $plugin_name,
+            $server_url,
+            $customer_key,
+            $customer_secret,
+            $product_ids,
+            $license_options,
+            $valid_object,
+            $ttl
+        )
+        {
 
-		# Initialize wp_args
-		$wp_args = [
-			'headers' => $headers,
-			'method' => $method,
-			'timeout' => 5
-		];
+            # Set plugin name for internationalization
+            $this->plugin_name = $plugin_name;
 
-		# Populate the args for use in the wp_remote_request call
-		if( !empty($args) ) {
-			$wp_args['body'] = $args;
-		}
+            # Connection variables
+            $this->api_url = "{$server_url}/wp-json/lmfwc/v2/";
+            $this->customer_key = $customer_key;
+            $this->customer_secret = $customer_secret;
 
-		# Make the call and store the response in $res
-		$res = wp_remote_request($url, $wp_args);
+            # Product IDs
+            $this->product_ids = is_array($product_ids) ? $product_ids : [$product_ids];
 
-		# Check for success
-		if(!is_wp_error($res) && ($res['response']['code'] == 200 || $res['response']['code'] == 201)) {
-			return json_decode($res['body'], TRUE);
-		}
-		elseif (is_wp_error($res)) {
-			throw new ErrorException( 'Unknown error', 500);
-		}
-		else {
-			$response = json_decode($res['body'], TRUE);
-			throw new ErrorException( $response['message'], $response['data']['status']);
-		}
-	}
+            # Get license key stored in the database
+            $this->stored_license = null;
+            if (isset($license_options['settings_key'])) {  // Check if WP Settings are used to store the license key
+                $license = get_option($license_options['settings_key']);
+                if ($license !== false) {
+                    $this->stored_license = $license[$license_options['option_key']];
+                }
+            } elseif (isset($license_options['option_key'])) {  // If no WP Settings are used to store the license key
+                $this->stored_license = get_option($license_options['option_key']);
+            }
 
-	/**
-	 * Activate license
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param $license_key
-	 * @return array|null
-	 * @throws ErrorException
-	 *
-	 */
-	public function activate($license_key) {
-		$license = null;
-		if( !empty($license_key) ) {
-			$response = $this->call( "licenses/activate/{$license_key}");
-			if( isset($response['success']) && $response['success'] === true ) {
-				$license = $response['data'];
-			}
-			else {
-				$this->valid_status['is_valid'] = false;
-				$this->valid_status['error'] = $response['message'];
-				$this->valid_status['nextValidation'] = time();
-				update_option($this->valid_object, $this->valid_status);
-				throw new ErrorException($response['message']);
-			}
-		}
+            # License variables
+            $this->valid_object = $valid_object;
+            $this->ttl = $ttl;
+            $this->valid_status = get_option($valid_object, []);
 
-		return $license;
-	}
+        }
 
-	/**
-	 * Deactivate license
-	 *
-	 * @since 1.0.0
-	 * @param $license_key
-	 * @throws ErrorException
-	 */
-	public function deactivate($license_key) {
-		if( !empty($license_key) ) {
-			$this->call( "licenses/deactivate/{$license_key}" );
-		}
-		delete_option($this->valid_object);
-	}
+        /**
+         * HTTP Request call
+         *
+         * @param string $endpoint
+         * @param string $method
+         * @param string $args
+         *
+         * @return array
+         * @throws ErrorException
+         * @since 1.0.0
+         *
+         */
+        private function call($endpoint, $method = 'GET', $args = '')
+        {
 
-	/**
-	 * Verify if the license is valid
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param $license_key
-	 * @return array
-	 *
-	 */
-	public function validate_status($license_key = '') {
+            # Populate the correct endpoint for the API request
+            $url = "{$this->api_url}{$endpoint}?consumer_key={$this->customer_key}&consumer_secret={$this->customer_secret}";
 
-		# Generic valid result
-		$valid_result = [
-			'is_valid' => false,
-			'error' => __('The license has not been activated yet', $this->plugin_name ),
-		];
+            # Create header
+            $headers = [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json; charset=UTF-8',
+            ];
 
-		$current_time = time();
+            # Initialize wp_args
+            $wp_args = [
+                'headers' => $headers,
+                'method' => $method,
+                'timeout' => 5
+            ];
 
-		# Use validation object if not force to validate
-		if( empty( $license_key ) && isset($this->valid_status['nextValidation']) && $this->valid_status['nextValidation'] > $current_time ) {
-			$valid_result['is_valid'] = $this->valid_status['is_valid'];
-			$valid_result['error'] = $this->valid_status['error'];
-		}
-		else {
+            # Populate the args for use in the wp_remote_request call
+            if (!empty($args)) {
+                $wp_args['body'] = $args;
+            }
 
-			# If no license send, look for the one stored in database
-			if ( empty( $license_key ) ) {
-				$license_key = $this->stored_license;
-			}
+            # Make the call and store the response in $res
+            $res = wp_remote_request($url, $wp_args);
 
-			# If there is no license
-			if ( empty( $license_key ) ) {
-				$valid_result['error'] = __( 'A license has not been submitted', $this->plugin_name );
-			}
-			else {
-				try {
-					$response = $this->call( "licenses/{$license_key}" );
-					if ( isset($response['success']) && $response['success'] === true ) {
+            # Check for success
+            if (!is_wp_error($res) && ($res['response']['code'] == 200 || $res['response']['code'] == 201)) {
+                return json_decode($res['body'], TRUE);
+            } elseif (is_wp_error($res)) {
+                throw new ErrorException('Unknown error', 500);
+            } else {
+                $response = json_decode($res['body'], TRUE);
+                throw new ErrorException($response['message'], $response['data']['status']);
+            }
+        }
 
-						# Calculate license expiration date
-						$this->valid_status['valid_until'] = ( $response['data']['expiresAt'] !== null ) ? strtotime( $response['data']['expiresAt'] ) : null;
+        /**
+         * Activate license
+         *
+         * @param $license_key
+         * @return array|null
+         * @throws ErrorException
+         *
+         * @since 1.0.0
+         *
+         */
+        public function activate($license_key)
+        {
+            $license = null;
+            if (!empty($license_key)) {
+                $response = $this->call("licenses/activate/{$license_key}");
+                if (isset($response['success']) && $response['success'] === true) {
+                    $license = $response['data'];
+                } else {
+                    $this->valid_status['is_valid'] = false;
+                    $this->valid_status['error'] = $response['message'];
+                    $this->valid_status['nextValidation'] = time();
+                    update_option($this->valid_object, $this->valid_status);
+                    throw new ErrorException($response['message']);
+                }
+            }
 
-						# If license key does not belongs to the Product id.
-						# if not Product id is defined, then this validation is omitted
-						if ( ! empty( $this->product_ids ) && ! in_array( $response['data']['productId'], $this->product_ids ) ) {
-							$valid_result['error'] = __( 'The license entered does not belong to this plugin', $this->plugin_name );
-						}
-						# Check that the license has not reached the expiration date
-						# if no expiration date is set, omit this
-						elseif ( $this->valid_status['valid_until'] !== null && $this->valid_status['valid_until'] < time() ) {
-							$valid_result['error'] = __( 'The license entered is expired', $this->plugin_name );
-						} else {
-							$valid_result['is_valid'] = true;
-							$valid_result['error'] = '';
-						}
+            return $license;
+        }
 
-					}
+        /**
+         * Deactivate license
+         *
+         * @param $license_key
+         * @throws ErrorException
+         * @since 1.0.0
+         */
+        public function deactivate($license_key)
+        {
+            if (!empty($license_key)) {
+                $this->call("licenses/deactivate/{$license_key}");
+            }
+            delete_option($this->valid_object);
+        }
 
-				}
-				catch ( ErrorException $exception ) {
-					$valid_result['error'] = $exception->getMessage();
-				}
-			}
-		}
+        /**
+         * Verify if the license is valid
+         *
+         * @param $license_key
+         * @return array
+         *
+         * @since 1.0.0
+         *
+         */
+        public function validate_status($license_key = '')
+        {
 
-		# Update validation object
-	$this->valid_status['nextValidation'] = strtotime(date('Y-m-d') . "+ {$this->ttl} days" );
-		$this->valid_status['is_valid'] = $valid_result['is_valid'];
-		$this->valid_status['error'] = $valid_result['error'];
-		update_option($this->valid_object, $this->valid_status);
+            # Generic valid result
+            $valid_result = [
+                'is_valid' => false,
+                'error' => __('The license has not been activated yet', $this->plugin_name),
+            ];
 
-		return $valid_result;
+            $current_time = time();
 
-	}
+            # Use validation object if not force to validate
+            if (empty($license_key) && isset($this->valid_status['nextValidation']) && $this->valid_status['nextValidation'] > $current_time) {
+                $valid_result['is_valid'] = $this->valid_status['is_valid'];
+                $valid_result['error'] = $this->valid_status['error'];
+            } else {
 
-	/**
-	 * Returns time of license validity
-	 *
-	 * @return int|null
-	 * @since 1.0.0
-	 */
-	public function valid_until() {
-		return isset($this->valid_status['valid_until']) ? $this->valid_status['valid_until'] : null;
-	}
+                # If no license send, look for the one stored in database
+                if (empty($license_key)) {
+                    $license_key = $this->stored_license;
+                }
+
+                # If there is no license
+                if (empty($license_key)) {
+                    $valid_result['error'] = __('A license has not been submitted', $this->plugin_name);
+                } else {
+                    try {
+                        $response = $this->call("licenses/{$license_key}");
+                        if (isset($response['success']) && $response['success'] === true) {
+
+                            # Calculate license expiration date
+                            $this->valid_status['valid_until'] = ($response['data']['expiresAt'] !== null) ? strtotime($response['data']['expiresAt']) : null;
+
+                            # If license key does not belongs to the Product id.
+                            # if not Product id is defined, then this validation is omitted
+                            if (!empty($this->product_ids) && !in_array($response['data']['productId'], $this->product_ids)) {
+                                $valid_result['error'] = __('The license entered does not belong to this plugin', $this->plugin_name);
+                            }
+                            # Check that the license has not reached the expiration date
+                            # if no expiration date is set, omit this
+                            elseif ($this->valid_status['valid_until'] !== null && $this->valid_status['valid_until'] < time()) {
+                                $valid_result['error'] = __('The license entered is expired', $this->plugin_name);
+                            } else {
+                                $valid_result['is_valid'] = true;
+                                $valid_result['error'] = '';
+                            }
+
+                        }
+
+                    } catch (ErrorException $exception) {
+                        $valid_result['error'] = $exception->getMessage();
+                    }
+                }
+            }
+
+            # Update validation object
+            $this->valid_status['nextValidation'] = strtotime(date('Y-m-d') . "+ {$this->ttl} days");
+            $this->valid_status['is_valid'] = $valid_result['is_valid'];
+            $this->valid_status['error'] = $valid_result['error'];
+            update_option($this->valid_object, $this->valid_status);
+
+            return $valid_result;
+
+        }
+
+        /**
+         * Returns time of license validity
+         *
+         * @return int|null
+         * @since 1.0.0
+         */
+        public function valid_until()
+        {
+            return isset($this->valid_status['valid_until']) ? $this->valid_status['valid_until'] : null;
+        }
+
+    }
 
 }
